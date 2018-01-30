4540f715aed82e96017dfb16aec5babea6defc21
Zhe Wang <auzwang@gmail.com>
2018-01-30 17:34:46 +1100

    add .editorconfig

--- index.js
+++ index.js
@@ -1,74 +1,74 @@
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize('test', 'zwang', null, {
-  host: 'localhost',
-  dialect: 'postgres',
+	host: 'localhost',
+	dialect: 'postgres',
 
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
+	pool: {
+		max: 5,
+		min: 0,
+		acquire: 30000,
+		idle: 10000
+	}
 
 });
 
 const primaryKey = {
-  id: {
-    type: Sequelize.UUID,
-    defaultValue: Sequelize.UUIDV4,
-    primaryKey: true
-  }
+	id: {
+		type: Sequelize.UUID,
+		defaultValue: Sequelize.UUIDV4,
+		primaryKey: true
+	}
 };
 
 const Project = sequelize.define('project', {
-  ...primaryKey
+	...primaryKey
 });
 
 const Route = sequelize.define('route', {
-  ...primaryKey,
-  userId: Sequelize.TEXT,
-  status: Sequelize.TEXT,
-  externalId: Sequelize.TEXT
+	...primaryKey,
+	userId: Sequelize.TEXT,
+	status: Sequelize.TEXT,
+	externalId: Sequelize.TEXT
 }, {
-  indexes: [
-    {
-      fields: ['projectId']
-    }
-  ]
+	indexes: [
+		{
+			fields: ['projectId']
+		}
+	]
 });
 
 Route.belongsTo(Project);
 
 const Stop = sequelize.define('stop', {
-  ...primaryKey,
-  location: Sequelize.GEOMETRY('POINT'),
-  etaSeconds: Sequelize.INTEGER,
-  status: Sequelize.TEXT,
-  externalId: Sequelize.TEXT
+	...primaryKey,
+	location: Sequelize.GEOMETRY('POINT'),
+	etaSeconds: Sequelize.INTEGER,
+	status: Sequelize.TEXT,
+	externalId: Sequelize.TEXT
 }, {
-  indexes: [
-    {
-      fields: ['routeId']
-    }
-  ]
+	indexes: [
+		{
+			fields: ['routeId']
+		}
+	]
 });
 
 Stop.belongsTo(Route);
 
 const syncTables = async () => {
-  await Project.sync();
-  await Route.sync();
-  await Stop.sync();
+	await Project.sync();
+	await Route.sync();
+	await Stop.sync();
 };
 
 (async () => {
-  try {
-    await sequelize.authenticate();
-    console.log('connected');
-    await syncTables();
-    console.log('tables sync');
-  } catch (err) {
-    console.log('unable to connect', err);
-  }
+	try {
+		await sequelize.authenticate();
+		console.log('connected');
+		await syncTables();
+		console.log('tables sync');
+	} catch (err) {
+		console.log('unable to connect', err);
+	}
 })();
