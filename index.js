af587ea2cbaec7c299db33fbe90fe8985e152411
Zhe Wang <zwang4@atlassian.com>
2023-08-03 16:06:51 +1000

    add in bad service

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+var express = require("express");
+
+var app = express();
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize("database", "fakeUser", "fakePassword", {
+	dialect: "sqlite",
+	storage: "data/fakeDB.sqlite",
+});
+
+app.post("/fakeLogin", function (req, res) {
+	sequelize.query(
+		"SELECT * FROM Products WHERE name LIKE " + req.body.username
+	);
+});
+
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize('name', 'credentialxxxx', null, {
